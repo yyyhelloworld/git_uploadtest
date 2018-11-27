362f327830240bd201471f42f2fbb50d423af5c0
yq <1006078789@qq.com>
2018-11-27 18:47:56 +0800

    测试list

---
+++ src/cn/qxfive/lambad/Test04.java
@@ -0,0 +1,11 @@
+package cn.qxfive.lambad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test04 {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("abc");
+    }
+}
