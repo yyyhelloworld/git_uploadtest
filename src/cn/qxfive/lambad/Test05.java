48e634cfb4c525ff6e7c18bb966fd89ff2f222f3
yq <1006078789@qq.com>
2018-12-05 22:45:29 +0800

    add BuilderTest

--- src/cn/qxfive/lambad/Test05.java
+++ src/cn/qxfive/lambad/Test05.java
@@ -4,5 +4,6 @@ public class Test05 {
     public static void main(String[] args) {
         TableRequest build = new TableRequest.Builder().setDeviceId("sf").setPassword("yq").build();
         System.out.println(build.toString());
+        System.out.println(23);
     }
 }
